ab077db6b434f24f49e0df55258c2c5c8de9e1aa
Rheno Rayhan <rayhanrheno@gmail.com>
2024-03-15 12:23:49 +0700

    W03: Jawaban Soal 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,14 @@ import Profile from "@/components/profile";
 export default function Home() {
   return (
     <section>
-      <Profile />
+      <center>
+      <h1>Ilmuwan yang luar biasa.</h1>
+      </center>
+      <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+        <Profile />
+        <Profile />
+        <Profile />
+      </div>
     </section>
   );
 }

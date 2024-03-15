486d865430bb95929deca116eccd8bbe64746eaf
Rheno Rayhan <rayhanrheno@gmail.com>
2024-03-15 13:18:11 +0700

    W03: Jawaban soal 6

--- src/components/todolist.tsx
+++ src/components/todolist.tsx
@@ -1,22 +1,24 @@
+const baseUrl = 'https://i.imgur.com/';
 const person = {
     name: 'Gregorio Y. Zara',
+    imageId: '7vQD0fP',
+    imageSize: 's',
     theme: {
       backgroundColor: 'black',
       color: 'pink'
-    },
-    url: "https://i.imgur.com/7vQD0fPs.jpg"
+    }
   };
   
   export default function TodoList() {
     return (
-        <div style={{}}>
+        <div style={person.theme}>
         <h1>
-          {"{"}person{"}"}'s Todos
+          {person.name}'s Todos
         </h1>
         <img
           className="avatar"
-          src="https://i.imgur.com/7vQD0fPs.jpg"
-          alt="Gregorio Y. Zara"
+          src={`${baseUrl}${person.imageId}${person.imageSize}.jpg`}
+          alt={person.name}
         />
         <ul>
           <li>Improve the videophone</li>
